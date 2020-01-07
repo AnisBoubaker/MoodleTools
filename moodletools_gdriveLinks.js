552a0797cbd679b6b8f502a01dfd795ddb09784f
Anis Boubaker <46570545+AnisBoubaker@users.noreply.github.com>
2020-01-07 15:44:42 -0500

    Added configuration, font awesome loading and docs.

--- moodletools_gdriveLinks.js
+++ moodletools_gdriveLinks.js
@@ -1,3 +1,42 @@
+/*
+This script converts a Google Drive file link into two links: 
+- One that shows a preview of the file within Google Drive
+- One giving direct download to the file.
+
+Ex.: <a href="https://drive.google.com/file/d/....">File description</a> will be converted into: 
+	(icon) File description [ Preview ] [ Download ]
+	Where Preview and Download will link to the corresponding file link.
+
+USAGE ON MOODLE: 
+----------------
+On any Moodle rich text editor where you'd like to convert your Google drive links: 
+- Add a link to the desired file, using the share link provided by Google Drive
+- If desired, you can add a class to the link to reflect the file type and a corresponding icon will be automatically added. 
+	The following classes are supported: 
+		* drive-pdf: PDF file
+		* drive-word: Word file
+		* drive-video: Video file 
+		* drive-image: Image file
+		* drive-zip: Archive file
+		* drive-code: Code file
+	Ex.: <a class="drive-video" href="https://drive.google.com/file/d/....">File description</a>
+
+	Note that if no class is specified, a default file icon will be added. 
+- Open the code editor
+- Place a call to this script at the end (replace ... by the URL where you've stored this script)
+<script src="...."></script>
+*/
+/***********************************************
+ *               CONFIG                        *
+ ***********************************************/
+drive_fontawsome_url = "https://kit.fontawesome.com/b64d736f71.js"
+preview_text = "Visualiser";
+download_text = "Télécharger";
+
+
+/***********************************************************
+ * Do not change below (unless you know what you're doing) *
+ ***********************************************************/
 function drive_onlyUnique(value, index, self)
 {
     return self.indexOf(value) === index;
@@ -43,6 +82,7 @@ function drive_getFileType(link_class)
 
 function drive_ConvertAllLinks()
 {
+	$.getScript( drive_fontawsome_url );
 	links = drive_getAllDriveLinks();
 	for (i = 0; i < links.length; i++) { 
 		link_text = links[i].text;
@@ -51,8 +91,8 @@ function drive_ConvertAllLinks()
 		show_link = links[i].href;
 		download_link = "https://drive.google.com/uc?export=download&id="+file_id;
 		append_text = "<span><i class='"+drive_getFileType(link_class)+"'></i> "+link_text+" ";
-		append_text+= "[ <a href=\""+show_link+"\">Visualiser</a> ] ";
-		append_text+= "[ <a href=\""+download_link+"\">Télécharger</a> ]</span>";
+		append_text+= "[ <a href=\""+show_link+"\"  target='_blank'>"+preview_text+"</a> ] ";
+		append_text+= "[ <a href=\""+download_link+"\">"+download_text+"</a> ]</span>";
 		$(links[i]).after(append_text);
 		links[i].parentNode.removeChild(links[i]);
 	}
